43f33b17f64f31d19063668624c221365bc3a712
FenianOdogwu <mokoro95@yahoo.com>
2022-08-19 04:48:55 +0100

    added new page components, added routing to respective components for home, portfolios, about me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: MainPageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    MainPageComponent,
+    ProjectPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
